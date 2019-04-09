85e5295dd0bcd2d0bac6128da1902bb02fa74925
emily baier <emilyrbaier@gmail.com>
2019-04-09 11:28:16 -0700

    improved client

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const net = require('net');
 
 //every client is a socket. called socket in docs
-const server = net.createServer(client=> {
+//clients are duplex streams
+const server = net.createServer(connectedClient=> {
     console.log('client connected!');
-    client.on('data', data=> {
-        console.log(data.toString());
+
+    connectedClient.on('data', data=> {
+        //when client writes a line, server does this: 
+        connectedClient.write(`ECHO FROM SERVER: ${data}`);
     });
 });
 
 //number is the port. port can go up to 60000. dont go under 1050
 server.listen(7890);
+//this is listening to new connections that come in
