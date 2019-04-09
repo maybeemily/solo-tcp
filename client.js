beb4e9ae2d74f3295c1ef001bc90bac775dda56f
emily baier <emilyrbaier@gmail.com>
2019-04-09 11:30:27 -0700

    class notes removed

--- client.js
+++ client.js
@@ -1,7 +1,4 @@
-//will replace telnet client
 const net = require('net');
-//command line tool that when we type a command it will fire an event.
-//beginnings of command line application.
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,21 +6,16 @@ const rl = readline.createInterface({
     prompt: '> '
 });
 
-//three way handshake. 
 const connectionToServer = net.createConnection(7890, '192.168.1.180', ()=> {
     console.log('connected!');
 
     rl.prompt();
-    //listening to line event - triggers sending the line to the server
     rl.on('line', line=> {
-    //after getting it, we print it then read prompt.
-    //this is a client of the server! 
         connectionToServer.write(line);
         rl.prompt();
     });
 });
 
-//when server sends back data it will trigger this event
 connectionToServer.on('data', data=> {
     console.log(`\n${data.toString()}`);
     rl.prompt();
